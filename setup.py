260301aa5d2f07844ca49b708cd82ad8249209b6
Xia Liu <xia.liu@canada.ca>
2020-11-12 19:29:04 -0600

    Update versioning to work with Conda

--- setup.py
+++ setup.py
@@ -33,27 +33,9 @@ if os.environ.get("BUILD_WITH_CYTHON"):
 with open("README.md") as fh:
     long_description = fh.read()
 
-def get_version():
-    filepath = os.path.join(os.path.dirname(__file__), "bgzip", "version.py")
-    if os.path.isfile(filepath):
-        # Pick up the version packaged into a wheel or source distribution
-        with open(filepath) as fh:
-            version = dict()
-            exec(fh.read().strip(), version)
-            return version['__version__']
-    else:
-        # Pick up version from git annotated tag (canonical version)
-        p = subprocess.run(["git", "describe", "--tags", "--match", "v*.*.*"], stdout=subprocess.PIPE)
-        p.check_returncode()
-        out = p.stdout.decode("ascii").strip()
-        if "-" in out:
-            out = out.split("-", 1)[0]
-        assert out.startswith("v")
-        return out[1:]
-
 setup(
     name='bgzip',
-    version=get_version(),
+    version='0.3.5dev0',
     description='Utilities working with blocked gzip streams.',
     long_description=long_description,
     long_description_content_type='text/markdown',
